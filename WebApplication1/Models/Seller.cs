6ff12caf04c43700c1ece73ca048a056b11f6167
andre236 <adr_hb@hotmail.com>
2022-03-17 01:01:11 -0300

    Simple Create Form

--- WebApplication1/Models/Seller.cs
+++ WebApplication1/Models/Seller.cs
@@ -8,13 +8,13 @@ namespace WebApplication1.Models
     public class Seller
     {
 
-        public int Id { get; private set; }
-        public string Name { get; private set; }
-        public string Email { get; private set; }
-        public DateTime BirthDate { get; private set; }
-        public double BaseSalary { get; private set; }
-        public ICollection<SalesRecord> Sales { get; private set; } = new List<SalesRecord>();
-        public Department Department { get; private set; }
+        public int Id { get;  set; }
+        public string Name { get;  set; }
+        public string Email { get;  set; }
+        public DateTime BirthDate { get;  set; }
+        public double BaseSalary { get;  set; }
+        public ICollection<SalesRecord> Sales { get;  set; } = new List<SalesRecord>();
+        public Department Department { get;  set; }
 
         public Seller()
         {
