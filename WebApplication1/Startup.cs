af90bca41f549fe5fd398b6977b3beec847db2b2
andre236 <adr_hb@hotmail.com>
2022-03-17 00:09:24 -0300

    SellerService and basic FindAll

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
+using WebApplication1.Services;
 
 namespace WebApplication1
 {
@@ -40,6 +41,7 @@ namespace WebApplication1
                     options.UseMySql(Configuration.GetConnectionString("WebApplication1Context"), builder => builder.MigrationsAssembly("WebApplication1")));
             
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
